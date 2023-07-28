9d53b8f8bad828049674947213ad87d21dcb43d3
Shevtsov Dmitry <shevtsov.dima.03@gmail.com>
2023-07-28 23:45:44 +0300

    Осуществлена первичная интеграция модуля программного расширения в структуру основной программы. Добавлены первичные функции проверки работоспособности библиотеки-расширения на реальной модели DWG.

--- AutocadCommandDWG/Command.cs
+++ AutocadCommandDWG/Command.cs
@@ -19,7 +19,34 @@ namespace AutocadCommandDWG
         [CommandMethod("SelectBlocks")]
         public void SelectBlocks()
         {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
 
+            PromptSelectionResult selectionResult = ed.GetSelection();
+            if (selectionResult.Status == PromptStatus.OK)
+            {
+                SelectionSet selectionSet = selectionResult.Value;
+                if (selectionSet.Count > 0)
+                {
+                    ed.WriteMessage("Были выбраны вхождения блоков.");
+                }
+                else
+                {
+                    ed.WriteMessage("Нет выбранных вхождений блоков.");
+                }
+            }
+            else if (selectionResult.Status == PromptStatus.Cancel)
+            {
+                ed.WriteMessage("Выбор отменен.");
+            }
+            else if (selectionResult.Status == PromptStatus.None)
+            {
+                ed.WriteMessage("Ничего не выбрано.");
+            }
+            else if (selectionResult.Status == PromptStatus.Error)
+            {
+                ed.WriteMessage("Ошибка при выборе.");
+            }
         }
     }
 }
