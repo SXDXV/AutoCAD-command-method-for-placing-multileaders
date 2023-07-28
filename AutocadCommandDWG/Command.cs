1a401d288c801404ae4150a92d34caf4a70b940c
Shevtsov Dmitry <shevtsov.dima.03@gmail.com>
2023-07-28 22:18:20 +0300

    Добавьте файлы проекта.

---
+++ AutocadCommandDWG/Command.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using System.Data;
+using System.Data.SQLite;
+using System.Xml.Linq;
+
+namespace AutocadCommandDWG
+{
+    public class Command
+    {
+        [CommandMethod("SelectBlocks")]
+        public void SelectBlocks()
+        {
+
+        }
+    }
+}
